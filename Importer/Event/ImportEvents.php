1e9a7e24c3c1021af86c035f4a60fcabc17284d8
senaria <sandro_hopf@gmx.de>
2016-07-05 11:20:06 +0200

    Import events are sorted (#37)
    Import events are sorted by the order they get executed

--- Importer/Event/ImportEvents.php
+++ Importer/Event/ImportEvents.php
@@ -3,9 +3,9 @@ namespace Netdudes\ImporterBundle\Importer\Event;
 
 final class ImportEvents
 {
-    const POST_BIND_DATA = 'EVENT_POST_BIND_DATA';
-
     const POST_FIELD_INTERPRET = 'EVENT_POST_FIELD_INTERPRET';
 
     const PRE_BIND_DATA = 'EVENT_PRE_BIND_DATA';
+
+    const POST_BIND_DATA = 'EVENT_POST_BIND_DATA';
 }
